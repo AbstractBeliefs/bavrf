5f2908e6a1abba9b1a24855cf3d90a457aaed325
Gareth Pulham <garethpulham@gmail.com>
2018-06-08 02:13:32 +0100

    Add no-op elimination (arith/navs with 0 values)

--- compiler/c/program_optimise.c
+++ compiler/c/program_optimise.c
@@ -38,8 +38,38 @@ node* combine_navigation(node* sequence){
     }
     return sequence;
 }
+
+node* eliminate_noops(node* sequence){
+    if (!sequence){ return sequence; }
+
+    node* prev = NULL;
+    node* curr = sequence;
+    node* next = NULL;
+
+    while (curr){
+        next = curr->next;
+        if ((curr->type == ARITHMETIC || curr->type == NAVIGATION) && curr->value == 0){
+            deleteNode(curr);
+            curr = next;
+            if (prev) {
+                prev->next = next;
+            } else {
+                sequence = next;
+            }
+            continue;
+        }
+        if (curr->type == LOOP){
+            curr->sub = eliminate_noops(curr->sub);
+        }
+        prev = curr;
+        curr = next;
+    }
+    return sequence;
+}
+
 node* (*optimisations[])(node*) = {
     &combine_arithmetic,
     &combine_navigation,
+    &eliminate_noops,
     NULL
 };
